eda422066f29046bc66100d73c1696338cbf64da
Ramish Lal Khokhar <sturamish@gmail.com>
2026-03-22 03:33:54 +0500

    W03 Team Activity complete

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,17 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Fraction f1 = new Fraction();
-        Fraction f2 = new Fraction(5);
-        Fraction f3 = new Fraction(3, 4);
+        Reference ref1 = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(ref1, "For God so loved the world");
 
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
-
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalValue());
-
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalValue());
+        Console.WriteLine(scripture.GetDisplayText());
     }
 }
---
+++ week03/Fractions/Scripture.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+
+        string[] parts = text.Split(" ");
+        foreach (string word in parts)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result;
+    }
+}
